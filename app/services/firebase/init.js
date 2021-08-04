82e38106a9a733dfac852b7681d961a5261b1615
Yogesh Kapuriya <ymkapuriya@users.noreply.github.com>
2021-08-04 19:30:40 +0530

    Update init.js

--- app/services/firebase/init.js
+++ app/services/firebase/init.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const FIREBASE_CONFIGS = {
-    apiKey: "AIzaSyBopyC4i6WnxlfYWi4GMb2iW-IrqbxZeY8",
-    authDomain: "expo-starter.firebaseapp.com",
-    databaseURL: "https://expo-starter.firebaseio.com",
+    apiKey: "#####",
+    authDomain: "#####",
+    databaseURL: "#####",
     projectId: "expo-starter",
-    storageBucket: "expo-starter.appspot.com",
-    messagingSenderId: "872207307326",
-    appId: "1:872207307326:web:964a732a0214119c73776f",
-    measurementId: "G-2CHNK6VPBQ"
+    storageBucket: "#####",
+    messagingSenderId: "#####",
+    appId: "#####",
+    measurementId: "#####"
 };
 
 firebase.initializeApp(FIREBASE_CONFIGS);
