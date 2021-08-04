7a42f36c7f053222780c66c7462ad5700697b7d8
Yogesh Kapuriya <ymkapuriya@users.noreply.github.com>
2021-08-04 19:36:15 +0530

    Update google.js

--- app/configs/google.js
+++ app/configs/google.js
@@ -3,5 +3,5 @@
 /**
  * Sign-in with google
  */
-export const ANDROID_CLIENT_ID = "872207307326-9q5r9tqljcu6dad2oo7i1n9caeg1rr1u.apps.googleusercontent.com";
-export const IOS_CLIENT_ID = "872207307326-lbrhb9ja0bro86ika3v88q9isv3rin2p.apps.googleusercontent.com";
+export const ANDROID_CLIENT_ID = "#";
+export const IOS_CLIENT_ID = "#";
