2574ad111fdf2e79c54cf836c9c876cd4908778e
Yogesh Kapuriya <ymkapuriya@users.noreply.github.com>
2021-08-04 19:35:39 +0530

    Update constants.js

--- app/configs/constants.js
+++ app/configs/constants.js
@@ -10,14 +10,14 @@ export const Data = {
     password: 'simple',
     deviceId: 'HA10T2AZ',
     userToken: {
-        access_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjNhNDFlYTY1ODIzODg5OTkyMzE2Y2Q0NDNlZTlmYzU2NzRjYTBkMzNmYTYzZGI5Mzg2MWRkYjE1ZWJmZDA2YWRjMzc1M2IyM2MyNWRkNDYwIn0.eyJhdWQiOiI1IiwianRpIjoiM2E0MWVhNjU4MjM4ODk5OTIzMTZjZDQ0M2VlOWZjNTY3NGNhMGQzM2ZhNjNkYjkzODYxZGRiMTVlYmZkMDZhZGMzNzUzYjIzYzI1ZGQ0NjAiLCJpYXQiOjE1ODk1NDI4ODksIm5iZiI6MTU4OTU0Mjg4OSwiZXhwIjoxNjIxMDc4ODg5LCJzdWIiOiIyMSIsInNjb3BlcyI6W119.fCPT9kj-CYc6ihfncjBskYh4QFApDC5BPBA6Z-02qhKEfgBGFk_eD1lCR85f3_klEYGTbSVjho2La_3ZnbND5MX9dBNa8sVhrbvTBWw0cayHdQ1St0vhQ4Mwwgoy1w9HQsrqq4oxWXKl406XST22GII0leeJkVDZ3jyeWeiCvKGTALvILM6Y4Nn2ckbymqFDmHypoDzGuh8XmTgQ0rADaPENYDY7yFoLDO2dltwBnF10KWJco_Qaw9mUYL0hC6IreO8Qziu6dmqKObZxZWW5XLEbhRC9Yvo9qVeohdqm-uJtlbkeXv9K_9hlSJmSsQmoxrAzMSDqTCIcTmM-tMeWoug8PHsfXp_75LPvzZm_z2CqQTBMILlJSzOtpCNmKqAbpHOU3UnSodogp5_zFBMjUuX9KXS0WVGQI3F9TPhemGuUOw7qz9SgTBzKJN3BfjJsJvZyU7V9DtLU3t77Bi0JGWsD83WcMRq-nteWGdUowp6hUNW2oKCbBjK6uHCaGKkPLV-LQFJ59g-GgsHZP2zeO0jYsfELTbvHBoeA5daOiiLTnb62B2WhwHe68mlPbf8an46p8QCpAiUNIUDPGzsSdl_BC1puwW1T0IXsPp-qp2M61HeEqbZYUPlUkQ2eTCjQZP3PPghuhLE_NsxuhuvV3Daow-WpTwQYIBVlqis16VU",
+        access_token: "#",
         expires_at: "2020-05-22 17:11:29",
-        role: "STCH_FINISHED_RECEIVER",
+        role: "ROLE",
         token_type: "Bearer"
     },
     subscriber: {
-        first_name: "Yogesh",
-        last_name: "Kapuriya",
+        first_name: "#",
+        last_name: "#",
         email: "email@email.com",
         password: "password",
         phone: "1234567890"
@@ -28,22 +28,22 @@ export const Data = {
         email: 'email@email.com'
     },
     googleSignIn: {
-        accessToken: "ya29.a0AfH6SMBI1Bq0h69CMGGFz5AAHeHfitpPk9xNI1q812EVe6hJ63fpMllYKJ9fIMK8y-XwpKIlTdDSp_CO4iZkykD205dXVbbl4j3UH9Lgn86OoZ7PqOQT6dsHSzUjDdVMDhmFUp8TM-_Xy6RVtWJOIV___Y0zkMbZJO8",
-        idToken: "eyJhbGciOiJSUzI1NiIsImtpZCI6ImMxNzcxODE0YmE2YTcwNjkzZmI5NDEyZGEzYzZlOTBjMmJmNWI5MjciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI4NzIyMDczMDczMjYtbGJyaGI5amEwYnJvODZpa2Ezdjg4cTlpc3YzcmluMnAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI4NzIyMDczMDczMjYtbGJyaGI5amEwYnJvODZpa2Ezdjg4cTlpc3YzcmluMnAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDM1ODc0MTQ2MDkwNTcyMjE3OTMiLCJlbWFpbCI6Inlta2FwdXJpeWFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJuaWdlUmJ2T3l4MzUyWmI0WVJkTVlBIiwibm9uY2UiOiJNdjEyT3Bad1lMT0FLRnA5WkVibkZTd2ZOb3RVZjMyNVhoWmVRRVdoVXNJIiwibmFtZSI6IllvZ2VzaCBLYXB1cml5YSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHaXRPOTN2VTNSV3dRbEYzTEdwaFlDMGItU2o4bzB0SncwcEFobTFIUT1zOTYtYyIsImdpdmVuX25hbWUiOiJZb2dlc2giLCJmYW1pbHlfbmFtZSI6IkthcHVyaXlhIiwibG9jYWxlIjoiZW4tR0IiLCJpYXQiOjE1ODk1Mzg2MDEsImV4cCI6MTU4OTU0MjIwMX0.Ks5Qg60_q5Z5dU4jP2uetYMOfLrqD-VTuszWXaOWcTPwCp8PDQZ7eD4-zDhhzyQV9VEjMiewOBkdw9EZRfpY22a00GzjjxpewW8vaFvk0CyvDlakR7BjDW61d-pVd79rHwXg-9jpZF_rUxv5YZlotVBewhpofr3RC39pr7YmE1CX64b0LfHdiQKhRU5cqqLfJRKpWbfx8AL9nPi_B17r0jJg-37T3ZEHoBDLaFsXpMFcBJbyjDrOsiGNzeMEnQ2n3hysxNvJ91d5tQgg9lQ1dpM007VndNcNov4umOKsTwRHscMxthk8NsmsP-MkfsB28tXSA5GIRACfHmhNrVnU9A",
-        refreshToken: "1//0g1rt6cg4EE82CgYIARAAGBASNwF-L9Ir8SZcV2Bk-2K3LdDaJLEOH9QQoto5OJoPpjbpGjjyPMK4TufNVji3cbSj6u3D7Wh8k8o",
+        accessToken: "#",
+        idToken: "#",
+        refreshToken: "#",
         type: "success",
         user: {
-            "email": "ymkapuriya@gmail.com",
-            "familyName": "Kapuriya",
-            "givenName": "Yogesh",
-            "id": "103587414609057221793",
-            "name": "Yogesh Kapuriya",
-            "photoUrl": "https://lh3.googleusercontent.com/a-/AOh14GitO93vU3RWwQlF3LGphYC0b-Sj8o0tJw0pAhm1HQ",
+            "email": "#",
+            "familyName": "#",
+            "givenName": "#",
+            "id": "#",
+            "name": "#",
+            "photoUrl": "#",
         }
     },
     firebaseAuth: {
-        email: "email@email.com",
-        password: "simple",
-        phone: "9925699877"
+        email: "#",
+        password: "#",
+        phone: "#"
     }
 }
